b472ff72ed4296ba9aa65e583a86cb5f032fac7d
Mohammed Ahmed Fulwala <48979024+mohful@users.noreply.github.com>
2025-10-25 21:49:35 -0400

    second commit

---
+++ get-quotes.py
@@ -0,0 +1,20 @@
+import requests
+import random
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
+
+def get_random_ayah():
+    surah = random.randint(1, 114)
+    ayah = random.randint(1, 7)
+    res = requests.get(f"https://api.alquran.cloud/v1/ayah/{surah}:{ayah}/en.asad")
+    data = res.json()["data"]
+    return {
+        "english": data["text"],
+        "translation": data["edition"]["englishName"],
+        "reference": f"{data['surah']['englishName']} {data['numberInSurah']}"
+    }
+
+if __name__ == "__main__":
+    a = get_random_ayah()
+    print(f"{a['english']}\n{a['translation']}\n{a['reference']}")
